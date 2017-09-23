becb0d3df7091574962d14f1ad753dd8c9b378d9
Harrison Brown <htb1@rice.edu>
2017-09-23 18:48:13 -0500

    added route for the song

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import send_file
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -8,8 +10,23 @@ app = Flask(__name__)
 #     return "Hello this from flask"
 
 @app.route("/")
-def hello():
+def homepage():
 	try:
 		return send_file('../client/index.html')
 	except Exception as e:
 		return str(e)
+
+@app.route('/song', methods = ['POST'])
+def process_url():
+
+	data = request.get_json()
+	song = data["song"]
+	print("received")
+	print(song)
+
+
+	# Might need to convert to Unicode
+
+	# Need to call processing function on song
+
+	return "NEED TO IMPLEMENT"
