e638e31e9db6bcbbdffbacbd7f96797a4d008482
Harrison Brown <htb1@rice.edu>
2017-09-23 21:21:56 -0500

    This is test to hook up backend server and front end server

--- server/server.py
+++ server/server.py
@@ -1,57 +1,30 @@
 from flask import Flask
 from flask import send_file
 from flask import request
-import json
-
-
-app = Flask(__name__)
+from getTimestampedGifs import get_timestamped_gifs
+app = Flask(_name_)
 
 # @app.route("/")
 # def hello():
 #     return "Hello this from flask"
 
+
 @app.route("/")
 def homepage():
 	try:
-		return send_file('../client/giftest.html')
+		return send_file('../client/index.html')
 	except Exception as e:
 		return str(e)
 
-# @app.route('/song', methods = ['POST'])
-# def process_url():
-
-# 	print(request.form)
-# 	data = request.get_json()
-# 	print(data)
-# 	x = json.loads(request.form)[0]
-# 	print(x)
-# 	# print(song)
-
-
-# 	# Might need to convert to Unicode
-
-# 	# Need to call processing function on song
-
-# 	return "NEED TO IMPLEMENT"
-
-
-@app.route('/test', methods = ['POST'])
-def saveget():
-	print("in process")
-	a=request.args.get('song', '')
-	print(a)
-
-	# print(request.form)
-	# data = request.get_json()
-	# print(data)
-	# x = json.loads(request.form)[0]
-	# print(x)
-	# print(song)
+@app.route('/song', methods = ['POST'])
+def process_url():
+	data = request.get_json()
+	song = data["song"]
+	print "song +++++++++++" + song
 
 
 	# Might need to convert to Unicode
 
 	# Need to call processing function on song
 
-	print("finished")
-	return "The name of the song is: " + a
+	return "hello"
